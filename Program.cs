7cc24957ef3a7783049d1bad29af35fba7ceb0ae
khraoverflow <brahmimohamedamine@hotmail.fr>
2022-06-08 03:53:46 +0100

    param parse attempt 1

--- Program.cs
+++ Program.cs
@@ -1,37 +1,149 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace khra_scan
 {
     class Program
     {
+
+        static int thread_count = 100;
+        static string start_address;
+        static string end_address;
+        static int port;
+        static int timeout;
+
+        static Dictionary<string,string> ParamsName = new Dictionary<string, string>();
         static void Main(string[] args)
         {
+
+            ParamsName.Add("-th","Thread count");
+            ParamsName.Add("-p","Port number to scan for");
+            ParamsName.Add("-t","timeout ");
+
+            Console.WriteLine("==================================================================");
+            Console.WriteLine("                  ----     NET scanner     ----");
+            Console.WriteLine("==================================================================");
             //Console.WriteLine("threads Subnet start_addr end_addrss port timeout");
             //Console.WriteLine("exemple: ");
             //Console.WriteLine("khra-scan.exe 200 192.168.1. 1 254 445 5");
             //Console.WriteLine("==================================================================");
-            Console.WriteLine("new format");
+            
+            /*Console.WriteLine("new format");
             Console.WriteLine("string Subnet,string start_sub, string end_sub ,string start_address,string end_address,int port,int timeout");
             Console.WriteLine("exemple");
             Console.WriteLine("khra-scan.exe 200 192.168. 135 235 1 254 445 3 ");
             Console.WriteLine("==================================================================");
+            */
+
             //HostScan scanner = new HostScan("192.168.1.", "1", "12",445,5);
 
             //HostScan scanner = new HostScan(args[1], args[2], args[3], int.Parse(args[4]), int.Parse(args[5]));
 
-            if(args.Length != 8)
-            {
-                Console.WriteLine("arguments error");
-                Environment.Exit(0);
-            }
-            HostScan scanner = new HostScan(args[1], args[2], args[3], args[4], args[5], int.Parse(args[6]), int.Parse(args[7]));
+          
+
+           // HostScan scanner = new HostScan(args[1], args[2], args[3], args[4], args[5], int.Parse(args[6]), int.Parse(args[7]));
 
    
 
-            scanner.start(int.Parse(args[0]));
+            //scanner.start(int.Parse(args[0]));
+            ParseMode(args);
+        }
+
+        static void ParseMode(string[] args)
+        {
+            string mode = args[0].ToLower();
+            
+              /*if(args.Length < 8)
+            {
+                Console.WriteLine("arguments error");
+                Environment.Exit(0);
+            }*/
+
+            switch (mode)
+            {
+                case "hosts":
+                    Console.WriteLine("starting host discovery ...");
+                    ParseParamVal(args);
+                    HostScanner();
+                    break;
+                case "ports":
+                    Console.WriteLine("starting port scanner On REPLACE THIS :");
+                    ParseParamVal(args);
+                    PortScanner();
+                    break;
+                case "-h" :
+                case "--help":
+                    Console.WriteLine("show help msg");
+                    break;
+                default:
+                    Console.WriteLine("command {0} not found try -h or--help",args[0]);
+                    break;
+            }
+        }
+
+        private static void ParseParamVal(string[] args)
+        {
+            /*Console.WriteLine("threads: " + ParamValue(args,"-Th"));
+            Console.WriteLine("range: " + ParamValue(args,"-R"));*/
+
+            //thread count
+            if (!int.TryParse(ParamValue(args,"-th",false,"200"),out thread_count))
+            {
+                Console.WriteLine(" [!] thread count should be an integer");
+                Environment.Exit(0);
+            }
+            
+            
+            //port
+            if(!int.TryParse(ParamValue(args,"-p",true,"0"),out port))
+            {
+                Console.WriteLine(" [!] port number must be integer");
+                Environment.Exit(0);
+            }
+            //timeout
+            if(!int.TryParse(ParamValue(args,"-t",false,"2"),out timeout))
+            {
+                Console.WriteLine(" [!] timeoute number must be integer");
+                Environment.Exit(0);
+            }
+            //range 
+
+            //TESTS
+            Console.WriteLine("threads: " + thread_count);
+            Console.WriteLine("port: " + port);
+            Console.WriteLine("timeout: " + timeout);
+            //Console.WriteLine("range: " + range);
+
+        }
+
+        static void HostScanner()
+        {
+
+        }
+
+        static void PortScanner()
+        {
+
+        }
+
+        static string ParamValue(string[] args,string param,bool IsMand,string def)
+        {
+            if(Array.IndexOf(args,param) == -1 )
+            {
+                if(IsMand)
+                {
+                    Console.WriteLine(" [!] Failed to find mandotory argument {0} for {1}",param, ParamsName[param]);
+                    Environment.Exit(0);
+                }
+               return def;
+            }
+            return args[Array.IndexOf(args,param) + 1];
         }
     }
 }
